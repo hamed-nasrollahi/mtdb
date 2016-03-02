c0d816389a2cb20c4492b0021fa2fc4e49559d79
vdemydiuk <vdemydiuk@gmail.com>
2016-03-02 12:00:41 +0200

    Added TestLibrary project

---
+++ TestLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TestLibrary
+{
+    class Program
+    {
+        #region Dll Import functions
+        [DllImport("MqlDbAdapter.dll")]
+        public static extern int init();        
+        #endregion
+
+        private static IntPtr _message = IntPtr.Zero;
+
+        static void Main(string[] args)
+        {
+            if (init() == 0)
+            {
+                Console.WriteLine("Init error: " + "MetaTraderApi");
+                return;
+            }
+
+            Console.WriteLine("Init success");
+        }
+    }
+}
